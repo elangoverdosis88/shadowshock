45c480c61d1c7b410e40a649e37e548aa7a1a803
elangoverdosis88 <34495547+elangoverdosis88@users.noreply.github.com>
2018-11-05 13:11:06 +0700

    Update shadow.sh

--- shadow.sh
+++ shadow.sh
@@ -27,44 +27,22 @@ cd
 
 
 
-# disable ipv6
-echo 1 > /proc/sys/net/ipv6/conf/all/disable_ipv6
-sed -i '$ i\echo 1 > /proc/sys/net/ipv6/conf/all/disable_ipv6' /etc/rc.local
 
 # install wget and curl
-apt-get update;apt-get -y install wget curl;
+apt-get update
+apt-get -y install wget curl
 apt-get install gem
 # set time GMT +7
 ln -fs /usr/share/zoneinfo/Asia/Jakarta /etc/localtime
 
-# set locale
-sed -i 's/AcceptEnv/#AcceptEnv/g' /etc/ssh/sshd_config
-service ssh restart
-
-# set repo
-wget -O /etc/apt/sources.list $source/debian7/sources.list.debian7
-wget http://www.dotdeb.org/dotdeb.gpg
-wget http://www.webmin.com/jcameron-key.asc
-cat dotdeb.gpg | apt-key add -;rm dotdeb.gpg
-cat jcameron-key.asc | apt-key add -;rm jcameron-key.asc
 
 
 
 
 
-# update
-apt-get update;apt-get -y upgrade;
 
-# install webserver
-apt-get -y install nginx php5-fpm php5-cli
-apt-get -y install zip tar
-apt-get install python
-cd
-# install essential package
-#echo "mrtg mrtg/conf_mods boolean true" | debconf-set-selections
-#apt-get -y install bmon iftop htop nmap axel nano iptables traceroute sysv-rc-conf dnsutils bc nethogs openvpn vnstat less screen psmisc apt-file whois ptunnel ngrep mtr git zsh mrtg snmp snmpd snmp-mibs-downloader unzip unrar rsyslog debsums rkhunter
-apt-get -y install bmon iftop htop nmap axel nano iptables traceroute sysv-rc-conf dnsutils bc nethogs less screen psmisc apt-file whois grep mtr git zsh unzip unrar rsyslog debsums rkhunter
-apt-get -y install build-essential
+
+
 
 # remove unused
 apt-get -y --purge remove samba*;
@@ -72,63 +50,31 @@ apt-get -y --purge remove apache2*;
 apt-get -y --purge remove sendmail*;
 apt-get -y --purge remove bind9*;
 
-#apt-get -y autoremove;
+
 
 # update
-apt-get update;apt-get -y upgrade;
 
-# install webserver
-apt-get -y install nginx php5-fpm php5-cli
-apt-get -y install zip tar
-apt-get install python
-cd
+
 
 # update apt-file
 apt-file update
 
 # update
-apt-get update;apt-get -y upgrade;
+apt-get update
+apt-get -y upgrade
 
 # install shadowshock
 apt-get install python
 apt-get install python-pip
 apt-get install sudo
 apt-get install git
-
+apt-get update
+apt-get -y upgrade
 
 pip install git+https://github.com/shadowsocks/shadowsocks.git@master
 # password shadowshock
 sudo ssserver -p 8388 -k 12345678 -m aes-256-cfb --user nobody -d start
 
-
-# install webserver
-cd
-rm /etc/nginx/sites-enabled/default
-rm /etc/nginx/sites-available/default
-wget -O /etc/nginx/nginx.conf $source/debian7/nginx.conf
-mkdir -p /home/vps/public_html
-echo "<pre>Modified by elang overdosis n' yusuf ardiansyah</pre>" > /home/vps/public_html/index.html
-echo "<?php phpinfo(); ?>" > /home/vps/public_html/info.php
-wget -O /etc/nginx/conf.d/vps.conf $source/debian7/vps.conf
-sed -i 's/listen = \/var\/run\/php5-fpm.sock/listen = 127.0.0.1:9000/g' /etc/php5/fpm/pool.d/www.conf
-service php5-fpm restart
-service nginx restart
-cd
-
-# install fail2ban
-apt-get update;apt-get -y install fail2ban;service fail2ban restart
-
-
-# install webmin
-cd
-# wget -O webmin-current.deb http://prdownloads.sourceforge.net/webadmin/webmin_1.870_all.deb
-wget -O webmin-current.deb $source/debian7/webmin-current.deb
-dpkg -i --force-all webmin-current.deb
-apt-get -y -f install;
-sed -i 's/ssl=1/ssl=0/g' /etc/webmin/miniserv.conf
-rm -f /root/webmin-current.deb
-apt-get -y --force-yes -f install libxml-parser-perl
-service webmin restart
 
 
 cd
@@ -141,32 +87,10 @@ echo "*/3 * * * * root /usr/bin/clearcache.sh" > /etc/cron.d/clearcache1
 
 cd
 
-# swap ram
-dd if=/dev/zero of=/swapfile bs=1024 count=4096k
-# buat swap
-mkswap /swapfile
-# jalan swapfile
-swapon /swapfile
-#auto star saat reboot
-wget $source/debian7/fstab
-mv ./fstab /etc/fstab
-chmod 644 /etc/fstab
-sysctl vm.swappiness=10
-#permission swapfile
-chown root:root /swapfile 
-chmod 0600 /swapfile
-cd
 
 
-# finishing
-chown -R www-data:www-data /home/vps/public_html
-service cron restart
-service nginx start
-service php5-fpm start
 
-service fail2ban restart
 
-service webmin restart
 
 cd
 rm -f /root/.bash_history && history -c
@@ -180,6 +104,6 @@ cd ~/
 rm -f /root/shadow.sh
 
 
-rm -f /root/IP
 
-echo 3 > /proc/sys/vm/drop_caches
+
+
