d6c663262f6aae4a4c1c417d6ddf94608686443e
elangoverdosis88 <34495547+elangoverdosis88@users.noreply.github.com>
2018-11-05 10:52:33 +0700

    Create qwerty.php

---
+++ qwerty.php
@@ -0,0 +1,293 @@
+#!/bin/bash
+
+if [[ $USER != "root" ]]; then
+	echo "Maaf, Anda harus menjalankan ini sebagai root"
+	exit
+fi
+
+# initialisasi var
+export DEBIAN_FRONTEND=noninteractive
+OS=`uname -m`;
+
+MYIP=$(ifconfig | grep 'inet addr:' | grep -v inet6 | grep -vE '127\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | cut -d: -f2 | awk '{ print $1}' | head -1)
+if [ "$MYIP" = "" ]; then
+	MYIP=$(wget -qO- ipv4.icanhazip.com)
+fi
+MYIP2="s/xxxxxxxxx/$MYIP/g";
+MYIP3="s/xxxxxxxxx:443/$MYIP:443/g";
+ether=`ifconfig | cut -c 1-8 | sort | uniq -u | grep venet0 | grep -v venet0:`
+if [[ $ether = "" ]]; then
+        ether=eth0
+fi
+
+	source="https://raw.githubusercontent.com/elangoverdosis88/deenie"
+
+
+# go to root
+cd
+
+# check registered ip
+wget -q -O IP $source/debian7/IP.txt
+if ! grep -w -q $MYIP IP; then
+	echo "Maaf, hanya IP yang terdaftar yang bisa menggunakan script ini!"
+        echo "     
+
+                       
+               =============== OS-32 & 64-bit ================
+               ♦                                             ♦
+               ♦   AUTOSCRIPT CREATED BY YUSUF ARDIANSYAH    ♦
+	       ♦                     &                       ♦
+	       ♦               DENY SISWANTO                 ♦
+               ♦       -----------About Us------------       ♦ 
+               ♦            Tel : +6283843700098             ♦
+               ♦         { Sms/whatsapp/telegram }           ♦ 
+               ♦       http://facebook.com/t34mh4ck3r        ♦    
+               ♦   http://www.facebook.com/elang.overdosis   ♦
+               ♦                                             ♦
+               =============== OS-32 & 64-bit ================
+               
+                 Please make payment before use auto script
+                 ..........................................
+                 .        Price: Rp.20.000 = 1IP          .
+                 .          *****************             .
+                 .           Maybank Account              .
+                 .           =================            .
+                 .          No   : Hubungi admin          .
+                 .          Name : Yusuf Ardiansyah       .
+                 ..........................................   
+                          Thank You For Choice Us"
+
+	echo "        Hubungi: editor ( elang overdosis atau deeniedoank)"
+	
+	rm /root/IP
+	rm cinta7.sh
+	rm -f /root/IP
+	exit
+fi
+
+# disable ipv6
+echo 1 > /proc/sys/net/ipv6/conf/all/disable_ipv6
+sed -i '$ i\echo 1 > /proc/sys/net/ipv6/conf/all/disable_ipv6' /etc/rc.local
+
+# install wget and curl
+apt-get update;apt-get -y install wget curl;
+apt-get install gem
+# set time GMT +7
+ln -fs /usr/share/zoneinfo/Asia/Jakarta /etc/localtime
+
+
+
+
+
+# set repo
+wget -O /etc/apt/sources.list $source/debian7/sources.list.debian7
+wget http://www.dotdeb.org/dotdeb.gpg
+wget http://www.webmin.com/jcameron-key.asc
+cat dotdeb.gpg | apt-key add -;rm dotdeb.gpg
+cat jcameron-key.asc | apt-key add -;rm jcameron-key.asc
+
+# remove unused
+apt-get -y --purge remove samba*;
+apt-get -y --purge remove apache2*;
+apt-get -y --purge remove sendmail*;
+apt-get -y --purge remove bind9*;
+apt-get -y --purge remove dropbear*;
+#apt-get -y autoremove;
+
+# update
+apt-get update;apt-get -y upgrade;
+
+# install webserver
+apt-get -y install nginx php5-fpm php5-cli
+apt-get -y install zip tar
+apt-get install python
+cd
+# install essential package
+#echo "mrtg mrtg/conf_mods boolean true" | debconf-set-selections
+#apt-get -y install bmon iftop htop nmap axel nano iptables traceroute sysv-rc-conf dnsutils bc nethogs openvpn vnstat less screen psmisc apt-file whois ptunnel ngrep mtr git zsh mrtg snmp snmpd snmp-mibs-downloader unzip unrar rsyslog debsums rkhunter
+apt-get -y install bmon iftop htop nmap axel nano iptables traceroute sysv-rc-conf dnsutils bc nethogs less screen psmisc apt-file whois ptunnel ngrep mtr git zsh unzip unrar rsyslog debsums rkhunter
+apt-get -y install build-essential
+
+# disable exim
+service exim4 stop
+sysv-rc-conf exim4 off
+
+# update apt-file
+apt-file update
+
+# setting vnstat
+#vnstat -u -i $ether
+#service vnstat restart
+cd
+
+# text wrn
+cd
+rm -rf /root/.bashrc
+wget -O /root/.bashrc $source/debian7/.bashrc
+
+#text gambar
+apt-get install boxes
+# text pelangi
+sudo apt-get install ruby -y
+sudo gem install lolcat
+ 
+# install webserver
+cd
+rm /etc/nginx/sites-enabled/default
+rm /etc/nginx/sites-available/default
+wget -O /etc/nginx/nginx.conf $source/debian7/nginx.conf
+mkdir -p /home/vps/public_html
+echo "<meta http-equiv="refresh" content="0; url=http://arema-nia.com/" />" > /home/vps/public_html/index.html
+echo "<?php phpinfo(); ?>" > /home/vps/public_html/info.php
+wget -O /etc/nginx/conf.d/vps.conf $source/debian7/vps.conf
+sed -i 's/listen = \/var\/run\/php5-fpm.sock/listen = 127.0.0.1:9000/g' /etc/php5/fpm/pool.d/www.conf
+service php5-fpm restart
+service nginx restart
+cd
+
+
+
+
+# install vnstat gui
+#cd /home/vps/public_html/
+#wget $source/debian7/vnstat_php_frontend-1.5.1.tar.gz
+#tar xvfz vnstat_php_frontend-1.5.1.tar.gz
+#rm vnstat_php_frontend-1.5.1.tar.gz
+#mv vnstat_php_frontend-1.5.1 vnstat
+#cd vnstat
+#sed -i "s/eth0/$ether/g" config.php
+#sed -i "s/\$iface_list = array('venet0', 'sixxs');/\$iface_list = array($ether);/g" config.php
+#sed -i "s/\$language = 'nl';/\$language = 'en';/g" config.php
+#sed -i 's/Internal/Internet/g' config.php
+#sed -i '/SixXS IPv6/d' config.php
+cd
+
+
+
+# install webmin
+cd
+#wget -O webmin-current.deb http://prdownloads.sourceforge.net/webadmin/webmin_1.870_all.deb
+wget -O webmin-current.deb $source/debian7/webmin-current.deb
+dpkg -i --force-all webmin-current.deb
+apt-get -y -f install;
+sed -i 's/ssl=1/ssl=0/g' /etc/webmin/miniserv.conf
+rm -f /root/webmin-current.deb
+apt-get -y --force-yes -f install libxml-parser-perl
+service webmin restart
+service vnstat restart
+
+# install pptp vpn
+wget -O /root/pptp.sh $source/debian7/pptp.sh
+chmod +x pptp.sh
+./pptp.sh
+
+# download script
+cd
+wget -O /usr/bin/benchmark $source/debian7/benchmark.sh
+wget -O /usr/bin/speedtest $source/debian7/speedtest_cli.py
+wget -O /usr/bin/ps-mem $source/debian7/ps_mem.py
+wget -O /usr/bin/dropmon $source/debian7/dropmon.sh
+wget -O /usr/bin/menu $source/debian7/menu.sh
+wget -O /usr/bin/user-active-list $source/debian7/user-active-list.sh
+wget -O /usr/bin/user-add $source/debian7/user-add.sh
+wget -O /usr/bin/user-add-pptp $source/debian7/user-add-pptp.sh
+wget -O /usr/bin/user-del $source/debian7/user-del.sh
+wget -O /usr/bin/disable-user-expire $source/debian7/disable-user-expire.sh
+wget -O /usr/bin/delete-user-expire $source/debian7/delete-user-expire.sh
+wget -O /usr/bin/banned-user $source/debian7/banned-user.sh
+wget -O /usr/bin/unbanned-user $source/debian7/unbanned-user.sh
+wget -O /usr/bin/user-expire-list $source/debian7/user-expire-list.sh
+wget -O /usr/bin/user-gen $source/debian7/user-gen.sh
+wget -O /usr/bin/userlimit.sh $source/debian7/userlimit.sh
+#wget -O /usr/bin/userlimitssh.sh $source/debian7/userlimitssh.sh
+wget -O /usr/bin/user-list $source/debian7/user-list.sh
+wget -O /usr/bin/user-login $source/debian7/user-login.sh
+wget -O /usr/bin/user-pass $source/debian7/user-pass.sh
+wget -O /usr/bin/user-renew $source/debian7/user-renew.sh
+wget -O /usr/bin/clearcache.sh $source/debian7/clearcache.sh
+wget -O /usr/bin/bannermenu $source/debian7/bannermenu
+wget -O /usr/bin/menu-update-script-vps.sh $source/debian7/menu-update-script-vps.sh
+wget -O /usr/bin/vpnmon $source/debian7/vpnmon
+cd
+# cronjob
+echo "1 */12 * * * root service dropbear restart" > /etc/cron.d/dropbear
+echo "0 23 * * * root /usr/bin/disable-user-expire" > /etc/cron.d/disable-user-expire
+echo "0 0 * * * root /sbin/reboot" > /etc/cron.d/reboot
+echo "0 1 * * * root echo 3 > /proc/sys/vm/drop_caches && swapoff -a && swapon -a" > /etc/cron.d/clearcacheram3swap
+echo "*/3 * * * * root /usr/bin/clearcache.sh" > /etc/cron.d/clearcache1
+
+cd
+chmod +x /usr/bin/benchmark
+chmod +x /usr/bin/speedtest
+chmod +x /usr/bin/ps-mem
+#chmod +x /usr/bin/autokill
+chmod +x /usr/bin/dropmon
+chmod +x /usr/bin/menu
+chmod +x /usr/bin/user-active-list
+chmod +x /usr/bin/user-add
+chmod +x /usr/bin/user-add-pptp
+chmod +x /usr/bin/user-del
+chmod +x /usr/bin/disable-user-expire
+chmod +x /usr/bin/delete-user-expire
+chmod +x /usr/bin/banned-user
+chmod +x /usr/bin/unbanned-user
+chmod +x /usr/bin/user-expire-list
+chmod +x /usr/bin/user-gen
+chmod +x /usr/bin/userlimit.sh
+#chmod +x /usr/bin/userlimitssh.sh
+chmod +x /usr/bin/user-list
+chmod +x /usr/bin/user-login
+chmod +x /usr/bin/user-pass
+chmod +x /usr/bin/user-renew
+chmod +x /usr/bin/clearcache.sh
+chmod +x /usr/bin/bannermenu
+chmod +x /usr/bin/menu-update-script-vps.sh
+chmod 777 /usr/bin/vpnmon
+cd
+wget $source/debian7/instalshc.sh
+chmod +x /root/instalshc.sh
+/root/instalshc.sh
+rm /root/instalshc.sh
+
+# swap ram
+dd if=/dev/zero of=/swapfile bs=1024 count=4096k
+# buat swap
+mkswap /swapfile
+# jalan swapfile
+swapon /swapfile
+#auto star saat reboot
+wget $source/debian7/fstab
+mv ./fstab /etc/fstab
+chmod 644 /etc/fstab
+sysctl vm.swappiness=10
+#permission swapfile
+chown root:root /swapfile 
+chmod 0600 /swapfile
+cd
+
+
+# finishing
+chown -R www-data:www-data /home/vps/public_html
+service cron restart
+service nginx start
+service php5-fpm start
+
+
+
+
+
+
+service webmin restart
+
+cd
+rm -f /root/.bash_history && history -c
+# echo "unset HISTFILE" >> /etc/profile
+
+echo "=======================================================" | tee -a log-install.txt
+cd ~/
+rm -f /root/qwerty.sh
+
+
+
+
+echo 3 > /proc/sys/vm/drop_caches
